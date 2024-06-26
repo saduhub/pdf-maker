803ec30567419854819529953c4b5880037b6713
Jorge Duarte <sadujorge@gmail.com>
2024-06-26 16:59:54 -0700

    Added connection to local mongo db.

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require("path");
+const db = require("./config/connection");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
@@ -13,7 +14,10 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+db.once("open", () => {
+    console.log("Connection to db successful");
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
 
